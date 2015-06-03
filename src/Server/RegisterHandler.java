a43f36b5c5e79617d11265749571e1ab738aef5e
Bernardo Gonçalves <bernardomfg@gmail.com / ei11071@fe.up.pt>
2015-06-04 00:26:28 +0100

    basic register with insert on database finished

--- src/Server/RegisterHandler.java
+++ src/Server/RegisterHandler.java
@@ -10,6 +10,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import static Server.Server.db;
+
 /**
  * Created by Bernardo on 01-06-2015.
  */
@@ -17,7 +19,9 @@ public class RegisterHandler implements HttpHandler {
 
     @Override
     public void handle(HttpExchange httpExchange) throws IOException {
-        JSONObject json;
+        JSONObject json = null;
+
+        String username = "", password = "", email = "";
 
         String method;
         method = httpExchange.getRequestMethod();
@@ -31,16 +35,30 @@ public class RegisterHandler implements HttpHandler {
         BufferedReader bfBody = new BufferedReader(new InputStreamReader(inputStream));
 
         System.out.println(httpExchange.getHttpContext().getPath());
-        System.out.println("request method: " + httpExchange.getRequestMethod());
+        System.out.println("request method: " + method);
 
         if (method.equals("POST")){
             if (headers.getFirst("Content-Type").equals("application/json")){
                 try {
                     json = new JSONObject(bfBody.readLine());
-                    System.out.println(json);
+                    username = json.get("username").toString();
+                    password = json.get("password").toString();
+                    email = json.get("email").toString();
+
+                    System.out.println("Username: " + username);
+                    System.out.println("Password (encrypted): " + password);
+                    System.out.println("Email: " + email);
+
+
+                    db.insertUsers(username, password, email);
+
                 } catch (Exception e) {
                     System.err.println(e.getClass().getName() + ": " + e.getMessage());
+
+                    //TODO: Tratamento de erros em todos os catches usando os status http como resposta;
+                    //TODO: Consultar http://www.w3.org/Protocols/rfc2616/rfc2616-sec10.html
                 }
+
 
 
             }
